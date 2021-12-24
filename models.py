21bf2090db7cf3fa2920947ac64eb6ccda5e4875
Luis Kirby M. Salva <luiskirby.salva@gmail.com>
2021-12-24 14:22:57 +0800

    Add user/multiple users

--- models.py
+++ models.py
@@ -1,8 +1,12 @@
+from re import fullmatch
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("sqlite:///users.db", echo=True)
+engine = create_engine("sqlite:///users.db", echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
 Base = declarative_base()
 
 
@@ -20,3 +24,17 @@ class User(Base):
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
+
+    # Adds a user
+    # kirby_user = User(name="Kirby", fullname="Luis Kirby Salva", nickname="Kirby")
+    # session.add(kirby_user)
+    # session.commit()
+
+    # Adds multiple users
+    # new_users = [
+    #     User(name="Marriane", fullname="Marriane Mones", nickname="Mols"),
+    #     User(name="Marjorie", fullname="Marjorie Mones", nickname="Marjo"),
+    #     User(name="Karen", fullname="Karen Tricia Salva", nickname="Karen"),
+    # ]
+    # session.add_all(new_users)
+    # session.commit()

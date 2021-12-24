ee9d6dfbd0a59bb68977c7a489754688625a038b
Luis Kirby M. Salva <luiskirby.salva@gmail.com>
2021-12-24 21:16:56 +0800

    Add query methods

--- models.py
+++ models.py
@@ -57,3 +57,20 @@ if __name__ == "__main__":
     # # Delete user
     # session.delete(user_to_delete) # replace user_to_delete with user object -> you can query to get hold of the user
     # session.commit() # Like creating and updating, changes must be commited before being reflected in the db
+
+    # # Querying
+    # user_table = session.query(User) # When you run this you get a query object back.
+    # This means querying the User class/table
+    # for user in user_table:
+    #   print(user)       # This will print each user object / row of the table
+    #   print(user.name)  # This will print each users' name
+    # session.query(User).order_by(User.name) # Will order the results alphabetically
+    # session.query(User).order_by(User.name).desc() # Now it's reverse alphabetically
+    # session.query(User).order_by(User.name)[:2] # This will return the first two users; Python implementation of LIMIT
+    # session.query(User).order_by(User.name)[2:] # This will return values after the first two users; Python implementation of OFFSET
+    # session.query(User).all() # Returns a list instead of a object/tuple
+    # session.query(User).order_by(User.name).first() # Returns first result of alphabetized Users
+    # session.query(User).filter_by(name='Jethro') # Grabs the user with name = 'Jethro'; filter_by uses key=value pairs
+    # session.query(User).filter(User.name == 'Jethro') # filter uses regular python operators
+    # You can combine/chain multiple filters
+    # Note that running queries will push data in your session (session.new or session.dirty) to the db. Try it! It's true.
